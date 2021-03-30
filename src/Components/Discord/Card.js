c200f5cc7b2212b3e29a2f0e1d1d7c695a614f5b
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-03-30 12:13:26 +0100

    Update Card.js

--- src/Components/Discord/Card.js
+++ src/Components/Discord/Card.js
@@ -1,1 +1,24 @@
+import React from "react";
 
+const Card = props => {
+  return (
+    <div className="card">
+      <div
+        className="card-bg"
+        style={{ backgroundImage: `url(${props.img})` }}
+      ></div>
+      <div className="grp">
+        <h2>{props.header}</h2>
+        <div
+          className={props.joined ? "joined-btn btn" : "join-btn btn"}
+          onClick={props.onClick}
+        >
+          {props.joined ? "Joined" : "Join"}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
+No
