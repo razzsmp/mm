a196ae45a43591e210a254be1a9446f237371db2
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-03-30 20:16:47 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,22 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./Reudux/Store";
 
+class ShowWindowDimensions extends React.Component {
+  state = { width: 0, height: 0 };
+  render() {
+    return <span>Window size: {this.state.width} x {this.state.height}</span>;
+  }
+  updateDimensions = () => {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  };
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
