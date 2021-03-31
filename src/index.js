1af434b68dd0026aef46aa5e91c44b22c21a1d57
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-03-31 20:56:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ ReactDOM.render(
   <Provider store={store}>
     <Browser
       <App />
+  <Route exacth path="/invite/:id" component={serverinvite} />
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
