8d8cf59ed50cca2ae469767c16d718ab5e1dac0f
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-03-31 21:19:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import { Provider } from "react-redux";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import store from "./Reudux/Store";
 import serverinvite from "./Components/Discord/Invite/invite";
 
