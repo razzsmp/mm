fc077d3d1d237b97abe43043b0efadcdf7308f3e
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-04-01 18:12:18 +0100

    Update SidePannel.js

--- src/Components/Discord/SidePannel/SidePannel.js
+++ src/Components/Discord/SidePannel/SidePannel.js
@@ -158,38 +158,6 @@ class SidePannel extends React.Component {
 					/>
 				) : null}
 			</div>
-                const { selectedServer, joinedServers } = this.props;
-		return (
-			<div className="sidepannel">
-				<Switch
-					active={selectedServer === null}
-					url="https://www.jimhub.gq/logo512.png"
-					onClick={() => {
-						this.props.changeCurrentSelected({
-							server: null
-						});
-					}}
-				/>
-				<div className="underline"></div>
-
-				{/* display joined servers */}
-				{joinedServers ? this.displayServers(joinedServers) : null}
-
-				<Add
-					onClick={() => this.setState({ showModal: true })}
-					url="https://cdn3.iconfinder.com/data/icons/stroke/53/Button-512.png"
-				/>
-				{this.state.showModal ? (
-					<JoinModel
-						create="Server"
-						show={this.setState.showModal}
-						onClick={this.handleCreateServer}
-						handleClose={() => this.setState({ showModal: false })}
-						status={this.state.createStatus}
-						percentage={this.state.percentage}
-					/>
-				) : null}
-			</div>
 		);
 	}
 };
