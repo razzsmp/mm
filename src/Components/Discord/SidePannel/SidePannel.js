912e9fcfc8cc845461c34c61c7d2bbe1b7fa17a8
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-04-01 14:30:23 +0100

    Update SidePannel.js

--- src/Components/Discord/SidePannel/SidePannel.js
+++ src/Components/Discord/SidePannel/SidePannel.js
@@ -2,6 +2,7 @@ import React from "react";
 import Server, { Switch, Add } from "./Server";
 import "./SidePannel.scss";
 import AddModal from "../AddModal/AddModal";
+import JoinModal from "../AddModal/joinmodal"
 import uuidv4 from "uuid/v4";
 
 class SidePannel extends React.Component {
@@ -148,6 +149,43 @@ class SidePannel extends React.Component {
 				/>
 				{this.state.showModal ? (
 					<AddModal
+						create="Server"
+						show={this.setState.showModal}
+						onClick={this.handleCreateServer}
+						handleClose={() => this.setState({ showModal: false })}
+						status={this.state.createStatus}
+						percentage={this.state.percentage}
+					/>
+				) : null}
+			</div>
+		);
+	}
+}
+
+	render() {
+		const { selectedServer, joinedServers } = this.props;
+		return (
+			<div className="sidepannel">
+				<Switch
+					active={selectedServer === null}
+					url="https://www.jimhub.gq/logo512.png"
+					onClick={() => {
+						this.props.changeCurrentSelected({
+							server: null
+						});
+					}}
+				/>
+				<div className="underline"></div>
+
+				{/* display joined servers */}
+				{joinedServers ? this.displayServers(joinedServers) : null}
+
+				<Add
+					onClick={() => this.setState({ showModal: true })}
+					url="https://cdn3.iconfinder.com/data/icons/stroke/53/Button-512.png"
+				/>
+				{this.state.showModal ? (
+					<JoinModel
 						create="Server"
 						show={this.setState.showModal}
 						onClick={this.handleCreateServer}
