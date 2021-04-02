968d60538c21b5dd2ebcca981978ff63d6107af7
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-04-02 21:00:17 +0100

    Update Channels.js

--- src/Components/Discord/Channels/Channels.js
+++ src/Components/Discord/Channels/Channels.js
@@ -25,6 +25,7 @@ class Channels extends React.Component {
 
 		return categories.map(category => {
 			const isAdmin = this.props.userRole.isAdmin;
+			const isOwner = this.props.user.uid
 			const channels = this.objToArray(category.channels);
 			return (
 				<div className="category" key={category.key}>
