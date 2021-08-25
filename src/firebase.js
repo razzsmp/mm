a7afd725822bea993413fcf609f9c63faea41c18
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-08-26 00:50:31 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import "firebase/storage";
 import "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAm0btAKMAiFnTLUO_YDZDJ7nm6673nJv4",
-  authDomain: "jimhnb.firebaseapp.com",
-  databaseURL: "https://jimhnb-default-rtdb.firebaseio.com",
-  projectId: "jimhnb",
-  storageBucket: "jimhnb.appspot.com",
-  messagingSenderId: "251597573789",
-  appId: "1:251597573789:web:0983db390549d44bb60e74",
-  measurementId: "G-JM4CPQNKTZ"
+  apiKey: "AIzaSyB7mgHqb4JFgYdk4vQvS3CTY9C6bNdeH9A",
+  authDomain: "jimhub0.firebaseapp.com",
+  databaseURL: "https://jimhub0-default-rtdb.firebaseio.com",
+  projectId: "jimhub0",
+  storageBucket: "jimhub0.appspot.com",
+  messagingSenderId: "416275417157",
+  appId: "1:416275417157:web:ac2723c9e787ddfd2f7b2c",
+  measurementId: "G-586HRWK1RK"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
