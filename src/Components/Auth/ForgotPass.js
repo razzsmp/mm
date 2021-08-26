77958a88e0c31268ec1e975c02b56d9894608086
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-08-26 01:26:13 +0100

    Update ForgotPass.js

--- src/Components/Auth/ForgotPass.js
+++ src/Components/Auth/ForgotPass.js
@@ -1,14 +1,49 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-class ForgotPassword extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Forgot Password Screen</Text>
-      </View>
-    )
-  }
-}
+const Login = props => {
+  const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState([]);
 
-export default ForgotPassword
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (isValid()) {
+      props.forgot(email);
+    }
+  };
+
+  const isValid = () => {
+    const errors = [];
+    if (!email.trim()) errors.push("email");
+    setErrors(errors);
+    return errors.length === 0;
+  };
+
+  return (
+    <div className="ForgotPass">
+      <h2>ForgotPassword</h2>
+      <h3>We expected you to forget it haha</h3>
+      <form onSubmit={handleSubmit}>
+        <label
+          style={{ color: errors.includes("email") ? "#d72323" : "white" }}
+        >
+          EMAIL
+        </label>
+        <input
+          type="email"
+          autoComplete="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          style={{
+            borderColor: errors.includes("email")
+              ? "#d72323"
+              : "rgba(0,0,0,0.2)"
+          }}
+        />
+
+        <button type="submit">Login</button>
+    </div>
+  );
+};
+
+export default ForgotPass;
