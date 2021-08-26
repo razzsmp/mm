34dc661ad3e0bd41040a279e9642e7fc68d3888f
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-08-26 01:16:28 +0100

    Update ForgotPass.js

--- src/Components/Auth/ForgotPass.js
+++ src/Components/Auth/ForgotPass.js
@@ -1,54 +1,14 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
 
-const Forgotpass = props => {
-	const [email, setEmail] = useState("");
-	const [errors, setErrors] = useState([]);
+class ForgotPassword extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Forgot Password Screen</Text>
+      </View>
+    )
+  }
+}
 
-	const handleSubmit = event => {
-		event.preventDefault();
-		if (isValid()) {
-			props.register(email);
-		} else {
-		}
-	};
-
-	const isValid = () => {
-		const emailRegex = /[a-zA-Z0-9]{4,}@[a-zA-Z0-9]{3,}\.[a-zA-Z]{1,10}/gi;
-		const errors = [];
-		if (!emailRegex.exec(email)) errors.push("email");
-		setErrors(errors);
-		return errors.length === 0;
-	};
-
-	return (
-		<div className="forgotpassword">
-			<h2>Forgot Password</h2>
-			<form onSubmit={handleSubmit}>
-				<label
-					style={{ color: errors.includes("email") ? "#d72323" : "white" }}
-				>
-					EMAIL
-				</label>
-				<input
-					type="email"
-					autoComplete="email"
-					value={email}
-					onChange={e => setEmail(e.target.value)}
-					style={{
-						borderColor: errors.includes("email")
-							? "#d72323"
-							: "rgba(0,0,0,0.2)"
-					}}
-				/>
-
-
-				<button type="submit">Continue</button>
-			</form>
-			{props.error ? <div className="error">{props.error.message}</div> : ""}
-			<Link to="/login">Already have an account?</Link>
-		</div>
-	);
-};
-
-export default Forgotpass;
+export default ForgotPassword
