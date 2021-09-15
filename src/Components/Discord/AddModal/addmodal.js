17ce2dbdd30f6240905c43a07887f6376b75a2c4
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-09-15 18:41:26 +0100

    Update addmodal.js

--- src/Components/Discord/AddModal/addmodal.js
+++ src/Components/Discord/AddModal/addmodal.js
@@ -49,7 +49,7 @@ const AddModal = props => {
 					backgroundImage: `url(${
 						file
 							? URL.createObjectURL(file)
-							: "https://www.jimhub.gq/logo512.png"
+							: "https://historialmc.xyz/server.png"
 					})`
 				}}
 			></label>
