57a7cc82bad85729ac7f0c011eebc6a98696453f
razzsmp <81592918+razzsmp@users.noreply.github.com>
2021-09-17 16:35:52 +0100

    Update UserSettings.js

--- src/Components/Discord/UserBar/UserSettings.js
+++ src/Components/Discord/UserBar/UserSettings.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import firebase from "../../firebase";
+import firebase from "../../../../firebase";
 
 const UserSettings = props => {
 	const [file, setFile] = useState(null);
