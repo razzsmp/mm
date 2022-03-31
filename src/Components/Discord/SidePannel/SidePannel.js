8307491d965894ff292514c8220393258be962ad
razzsmp <81592918+razzsmp@users.noreply.github.com>
2022-03-31 21:23:54 +0100

    Update SidePannel.js

--- src/Components/Discord/SidePannel/SidePannel.js
+++ src/Components/Discord/SidePannel/SidePannel.js
@@ -145,7 +145,7 @@ class SidePannel extends React.Component {
 
 				<Add
 					onClick={() => this.setState({ showModal: true })}
-					url="https://historialmc.xyz/add.png"
+					url="https://github.com/razzsmp/mm/blob/master/public/apps.png"
 				/>
 				{this.state.showModal ? (
 					<AddModal
